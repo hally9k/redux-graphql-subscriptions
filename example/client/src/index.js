18e9142de7d8ec1b9dd30c503d08e99c13928d4b
hally9k <hally9k@gmail.com>
2018-11-29 15:02:33 +1300

    Polished release deps

--- example/client/src/index.js
+++ example/client/src/index.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './app'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { applyMiddleware, createStore } from 'redux'
-import reducer from './reducer'
-import { Provider } from 'react-redux'
-import createGraphQLSubscriptionsMiddleware from 'redux-graphql-subscriptions'
-import { createLogger } from 'redux-logger'
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./app";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+import { createMiddleware } from "redux-graphql-subscriptions";
+import { createLogger } from "redux-logger";
 
-const subscriptionsUrl = 'ws://localhost:8081/subscriptions'
+const subscriptionsUrl = "ws://localhost:8081/subscriptions";
 const subscriptionOptions = {
-    reconnect: true,
-    connectionParams: {
-        token: '^$%%$@^%^O*^&UE$%#T%Y'
-    }
-}
+  reconnect: true,
+  connectionParams: {
+    token: "^$%%$@^%^O*^&UE$%#T%Y"
+  }
+};
 
 const enhancer = composeWithDevTools(
-    applyMiddleware(
-        createGraphQLSubscriptionsMiddleware(
-            subscriptionsUrl,
-            subscriptionOptions
-        ),
-        createLogger()
-    )
-)
+  applyMiddleware(
+    createMiddleware(subscriptionsUrl, subscriptionOptions),
+    createLogger()
+  )
+);
 
-const store = createStore(reducer, enhancer)
+const store = createStore(reducer, enhancer);
 
 const rootElement: HTMLElement =
-    document.getElementById('root') || document.getElementsByTagName('BODY')[0]
+  document.getElementById("root") || document.getElementsByTagName("BODY")[0];
 
 ReactDom.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootElement
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
