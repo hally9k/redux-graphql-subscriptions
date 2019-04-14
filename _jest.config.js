2544c0c0d3774a7c421adeccef94bf7742af9826
hally9k <hal@vitaler.com>
2019-04-14 14:20:03 +1200

    Tody up files

--- _jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  modulePaths: ['<rootDir>/node_modules/'],
-  roots: ['<rootDir>/src/'],
-  testRegex: '(\\.(test|spec))\\.(ts|tsx)$',
-}
